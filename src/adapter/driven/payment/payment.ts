24d5654dde19823f03a3715f72103532762dc840
tsombra <tsombra@questrade.com>
2024-01-26 12:29:40 -0300

    prettier: format payment

--- src/adapter/driven/payment/payment.ts
+++ src/adapter/driven/payment/payment.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateProductDto } from '../../../@core/domain/dto/update-product.dto';
-import { IPayment } from '../../../@core/ports/ipayment'
-
+import { IPayment } from '../../../@core/ports/ipayment';
 
 @Injectable()
 export class PaymentMercadoPago implements IPayment {
-     update(id: string, product: UpdateProductDto): boolean {
-        return true
-    }
-
+  update(id: string, product: UpdateProductDto): boolean {
+    return true;
+  }
 }
