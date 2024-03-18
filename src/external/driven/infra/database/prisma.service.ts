66210b5ca7e413dfb2749b85aa977c9323681fbb
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-18 09:52:50 -0300

    fix: fix database url

--- src/external/driven/infra/database/prisma.service.ts
+++ src/external/driven/infra/database/prisma.service.ts
@@ -1,20 +1,20 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { url } from './prisma';
+// import { url } from './prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(PrismaClient.name);
 
-  public constructor() {
-    super({
-      datasources: {
-        db: {
-          url,
-        },
-      },
-    });
-  }
+  // public constructor() {
+  //   super({
+  //     datasources: {
+  //       db: {
+  //         url,
+  //       },
+  //     },
+  //   });
+  // }
 
   async onModuleInit(): Promise<void> {
     await this.$connect();
