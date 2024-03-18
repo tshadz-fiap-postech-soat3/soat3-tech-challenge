8f052ede7321f9fabe34d659d48f1ef8d8a29d5f
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-18 12:03:33 -0300

    fix: add some logs

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "Iniciando migrações..."
 # Aplicar as migrações
-npx prisma migrate deploy
-
+npx npx prisma migrate dev --name init && npx ts-node prisma/seed.ts
+echo "Migrações concluídas."
 # Iniciar o servidor
 npm start
