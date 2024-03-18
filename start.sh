7719e9dbab78a16e0059e7cab434cb2d57b10008
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-18 15:03:07 -0300

    remove echo messages

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/ash
 
-echo "Iniciando migrações..."
 # Aplicar as migrações
 npx prisma migrate dev --name init && npx ts-node prisma/seed.ts
-echo "Migrações concluídas."
 # Iniciar o servidor
 npm start
